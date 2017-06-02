c341805c933da0eac25e8ac257e2f30a739d899b
Lukas Juhas <hello@lukasjuhas.com>
2017-06-02 12:23:11 +0100

    handle plugins, general improvements, wip

--- app/index.js
+++ app/index.js
@@ -13,16 +13,49 @@ const unwantedFiles = [
   '/wp-content/plugins/akismet',
 ];
 
+const plugins = {
+  acf: 'Advanced Custom Fields PRO',
+  yoast: 'Yoast SEO',
+  regeneratethumbs: 'Regenrate Thumbnails',
+  querymonitor: 'Query Monitor',
+  smushit: 'WP Smush it',
+  disablecomments: 'Disable Comments',
+  cf7: 'Contact Form 7',
+};
+
+const pluginNames = {
+  acf: 'advanced-custom-fields-pro',
+  yoast: 'wordpress-seo',
+  regeneratethumbs: 'regenerate-thumbnails',
+  querymonitor: 'query-monitor',
+  smushit: 'wp-smushit',
+  disablecomments: 'disable-comments',
+  cf7: 'contact-form-7',
+};
+
+const pluginZipballs = {
+  plugin_acf: 'https://connect.advancedcustomfields.com/index.php?p=pro&a=download&k=',
+  plugin_yoast: 'https://downloads.wordpress.org/plugin/wordpress-seo.zip',
+  plugin_regeneratethumbs: 'https://downloads.wordpress.org/plugin/regenerate-thumbnails.zip',
+  plugin_querymonitor: 'https://downloads.wordpress.org/plugin/query-monitor.zip',
+  plugin_smushit: 'https://downloads.wordpress.org/plugin/wp-smushit.zip',
+  plugin_disablecomments: 'https://downloads.wordpress.org/plugin/disable-comments.zip',
+  plugin_cf7: 'https://downloads.wordpress.org/plugin/contact-form-7.zip',
+};
+
 const config = {
   dir: './wp',
   tmp: './.tmp',
+  template: './template',
   wpZipball: 'https://wordpress.org/latest.zip',
   barebonesZipball: 'https://github.com/benchmarkstudios/barebones/archive/master.zip',
 };
 
+let afcLicence = null;
+
 const log = console.log;
 
-const zipinator = (zipballUri = false, target = config.tmp, localName = 'zipinator', entry = false, callback = false) => {
+const zipinator = (zipballUri = false, target = config.tmp, localName = 'zipinator', entry = localName, callback = false) => {
   if (!zipballUri) {
     return false;
   }
@@ -32,9 +65,15 @@ const zipinator = (zipballUri = false, target = config.tmp, localName = 'zipinat
   const local = `${localName}.zip`;
   const output = fs.createWriteStream(`${config.tmp}/${local}`);
 
+  let uri = zipballUri;
+  // attach licence if acf
+  if (localName === 'advanced-custom-fields-pro') {
+    uri = zipballUri + afcLicence;
+  }
+
   const req = request({
     method: 'GET',
-    uri: zipballUri,
+    uri,
     headers: {
       'Accept-Encoding': 'gzip,deflate,sdch',
     },
@@ -68,21 +107,85 @@ const zipinator = (zipballUri = false, target = config.tmp, localName = 'zipinat
   });
 };
 
+const readmenator = (projectName) => {
+  const file = 'README.md';
+  fs.readFile(`${config.template}/${file}`, 'utf8', (err, data) => {
+    log(`✏️ Generating ${file} file..`);
+    if (err) {
+      return log(err);
+    }
+    const result = data.replace(/{{ name }}/g, projectName);
+    return fs.writeFile(`${config.dir}/${file}`, result, (err) => {
+      if (err) {
+        return log(err);
+      }
+
+      return true;
+    });
+  });
+};
+
+const pluginator = (plugins) => {
+  Object.keys(plugins).forEach((key) => {
+    if (plugins[key]) {
+      zipinator(pluginZipballs[key], `${config.dir}/wp-content/plugins/${pluginNames[key.replace('plugin_', '')]}`, pluginNames[key.replace('plugin_', '')]);
+    }
+  });
+};
+
 module.exports = class extends Generator {
-  consstructor(args) {
-    this.projectName = args || 'barebones';
+  constructor(args, opts) {
+    super(args, opts);
+
+    this.argument('projectName', { type: String, required: true });
+  }
+
+  prompting() {
+    const items = [];
+    Object.keys(plugins).forEach((key) => {
+      items.push({
+        type: 'confirm',
+        name: `plugin_${key}`,
+        message: `Would you like to install ${plugins[key]} plugin?`,
+      });
+    });
+
+    return this.prompt(items).then((answers) => {
+      this.plugins = answers;
+
+      if (answers.plugin_acf) {
+        this.log('\n');
+        return this.prompt([{
+          type: 'input',
+          name: 'plugin_acf_licence',
+          message: 'Enter your Advanced Custom Fields licence key',
+          default: '',
+        }]).then((answer) => {
+          afcLicence = answer.plugin_acf_licence;
+          this.plugins.plugin_acf_licence = answer.plugin_acf_licence;
+        });
+      }
+
+      return this.plugins;
+    });
   }
 
   barebones() {
-    // zipinator(config.wpZipball, config.dir, 'wordpress', 'wordpress', () => {
-    //   this.log('🚧 Cleaning up unwanted files..');
-    //   unwantedFiles.forEach((file) => {
-    //     rimraf(config.dir + file, () => {
-    //       this.log(`🔥 ${file}`);
-    //     });
-    //   });
+    this.log('\nCool, setting you up! 🚀\n');
 
-      zipinator(config.barebonesZipball, `${config.dir}/wp-content/themes/${this.projectName}`, 'barebones', 'barebones-master');
-    // });
+    zipinator(config.wpZipball, config.dir, 'wordpress', 'wordpress', () => {
+      this.log('🚧 Cleaning up unwanted files..');
+      unwantedFiles.forEach((file) => {
+        rimraf(config.dir + file, () => {
+          this.log(`🔥 ${file}`);
+        });
+      });
+
+      zipinator(config.barebonesZipball, `${config.dir}/wp-content/themes/${this.options.projectName}`, 'barebones', 'barebones-master');
+
+      readmenator(this.options.projectName);
+
+      pluginator(this.plugins);
+    });
   }
 };
